3cee8cea6c83d32b838e4629af1012e60721f79c
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-06-06 13:09:49 +0530

    add basic admin and user dashboard page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+const Admin = () => {
+  return (
+    <>
+      <div className="mx-auto mt-6 max-w-6xl">
+        <h2 className="text-centr text-3xl">Admin</h2>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
---
+++ src/app/user/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const UserDashboard = () => {
+  return (
+    <>
+      <div className="mx-auto mt-6 max-w-6xl">
+        <h2 className="text-centr text-3xl">User Dashboard</h2>
+      </div>
+    </>
+  );
+};
+
+export default UserDashboard;
